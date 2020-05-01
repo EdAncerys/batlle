a5989896df37d4b283d5eaff9fab0a1f054968e4
Ed <lookatemail@gmail.com>
2020-05-01 15:05:35 +0100

    I want Player 2 to attack me, and I want to get a confirmation

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -30,4 +30,12 @@ feature 'Attacking' do
     expect(page).to have_content 'Dave: 60HP'
   end
 
+  scenario 'confirmation on other players attack' do
+    sign_in_and_play
+    click_link 'Attack'
+    click_link 'OK'
+    click_link 'Attack'
+    expect(page).to have_content 'Dave attacked Mittens'
+  end
+
 end
